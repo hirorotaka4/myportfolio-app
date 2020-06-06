35c0169bd65a4c853f3b40855c03b55f4b0b3257
hirorotaka4 <hirorotaka4@icloud.com>
2020-06-06 22:27:15 +0900

    listNew モーダルウインドウ実装

---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".signUp-show").click(function () {
+    $("#newList-modal").fadeIn();
+  });
+  $(".close-modal").click(function () {
+    $("#newList-modal").fadeOut();
+  });
+});
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,6 @@ class TopController < ApplicationController
   def index
     @lists = List.where(user: current_user).order("created_at ASC")
     @now = Time.now
+    @list = List.new
   end
 end
