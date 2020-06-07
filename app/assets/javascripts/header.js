12b089353fe9eb22d8adf53923357caec9ed50d0
hirorotaka4 <hirorotaka4@icloud.com>
2020-06-07 23:24:26 +0900

    update link_to jquery実装

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".nav-link").hover(
+    function () {
+      $(this).css("color", "#333300");
+    },
+    function () {
+      $(this).css("color", "#fff");
+    }
+  );
+});
--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -5,4 +5,31 @@ $(function () {
   $(".close-modal").click(function () {
     $("#newList-modal").fadeOut();
   });
+  $(".addCard_link").hover(
+    function () {
+      $(this).animate(
+        {
+          "font-size": "30px",
+        },
+        200
+      );
+    },
+    function () {
+      $(this).animate(
+        {
+          "font-size": "24px",
+        },
+        200
+      );
+    }
+  );
+
+  $(".listIcon").hover(
+    function () {
+      $(this).css("color", "red");
+    },
+    function () {
+      $(this).css("color", "#8c8c8c");
+    }
+  );
 });
